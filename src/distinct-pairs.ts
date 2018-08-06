001ae73fe0a4e3d29a55aab5f521056f012ccc62
Angelica Pisiritzi <apisiritzi@gmail.com>
2018-08-06 12:20:01 +0100

    Add files via upload

--- src/distinct-pairs.ts
+++ src/distinct-pairs.ts
@@ -1,4 +1,20 @@
 export function distinctPairs(items: Array<number>, target: number): number {
   // TODO: implement me
-  return 0
+
+  	var pairs = {};
+  	var count = 0;
+
+	for (var i in items){
+		for (var j in items){
+			if(items[i] + items[j] == target){
+				if(pairs[items[i]] == undefined && pairs[items[j]] == undefined){
+					count++;
+					pairs[items[i]] = true;
+					pairs[items[j]] = true;
+				}
+			}
+		}
+	}
+
+  	return count;
 }
--- src/merge-strings.ts
+++ src/merge-strings.ts
@@ -1,4 +1,14 @@
 export function mergeStrings(a: string, b: string): string {
-  // TODO: implement me
-  return ''
+  	// TODO: implement me
+
+	var res='';
+
+	for (var i in a){
+		res+= a[i];
+		if(b[i] != undefined) res+=b[i];
+	}
+
+	if(a.length < b.length) res+=b.substr(a.length, b.length);
+	
+  	return res;
 }
